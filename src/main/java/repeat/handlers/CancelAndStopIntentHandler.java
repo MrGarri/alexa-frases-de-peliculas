652d5f8e6d03780a05aa255a0469097ef539b889
Roberto Garrido <roberto.garrido@ctb.upm.es>
2018-11-26 19:59:00 +0100

    Add cancel and stop handler

--- src/main/java/repeat/handlers/CancelAndStopIntentHandler.java
+++ src/main/java/repeat/handlers/CancelAndStopIntentHandler.java
@@ -1,4 +1,31 @@
-package repeat.handlers;
+package handlers;
 
-public class CancelAndStopIntentHandler {
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import repeat.ResponseStreamHandler;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class CancelAndStopIntentHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(
+                intentName("AMAZON.StopIntent")
+                        .or(intentName("AMAZON.CancelIntent"))
+                        .or(intentName("AMAZON.NoIntent"))
+        );
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        return input.getResponseBuilder()
+                .withSpeech("Adiós")
+                .withSimpleCard(ResponseStreamHandler.SKILL_TITLE, "Adiós")
+                .withShouldEndSession(true)
+                .build();
+    }
 }
